daecacfd79f753d1607e4031c8fd0297f1c3c824
Lejdi <lejdi1618@gmail.com>
2020-03-30 21:37:46 +0200

    Draw layouts

---
+++ app/src/main/java/pl/lejdi/gymdiary/MainActivity.kt
@@ -0,0 +1,31 @@
+package pl.lejdi.gymdiary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.new_exercise -> {
+                Toast.makeText(this,"Item Clicked",Toast.LENGTH_SHORT).show();
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+}
